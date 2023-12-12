d9d38bb46740936087e224f1d5c03fdd5d528314
Himanshu-AT <aristech.official@gmail.com>
2023-12-12 21:10:47 +0530

    Completed

--- src/utils/index.ts
+++
@@ -1,9 +0,0 @@
-import { compareDesc, parseISO } from "date-fns";
-
-export const cx = (...classNames: string[]) => classNames.filter(Boolean).join(" ");
-
-export const sortBlogs = (blogs: any[]) => {
-    return blogs.slice().sort((a, b) =>
-        compareDesc(parseISO(a.publishedAt), parseISO(b.publishedAt))
-    );
-};
---
+++ src/utils/siteMetaData.tsx
@@ -0,0 +1,18 @@
+const siteMetadata = {
+  title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+  author: 'Himanshu',
+  headerTitle: 'Next.js Blog',
+  description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+  language: 'en-us',
+  theme: 'light',
+  siteUrl: 'https://blog-portfolio.vercel.app',
+  siteLogo: 'public/images/profile-img.jpeg',
+  socialBanner: '/social-banner.png',
+  email: 'hyattherate2005@gmail.com',
+  github: 'https://github.com/Himasnhu-at',
+  twitter: 'https://twitter.com/himansh806',
+  linkedin: 'https://www.linkedin.com/in/Himanshu-at/',
+  locale: 'en-US',
+}
+
+export default siteMetadata
